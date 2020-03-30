3992271e88e3ee2a25d34edde632bcac43b82d67
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-30 23:37:28 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,8 @@
-# Write your code here
+# Build a lopp that counts from 10 down to 1, outputting the curernt valud during each loop.
+#After 1 is displayed the system will display Happy New Year!
+magic_exit_number = 1
+count = 0  #we have to start at 0
+while count < 10 
+  puts count
+  count = count += 1
+end
